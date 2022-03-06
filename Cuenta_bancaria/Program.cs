5045372236c94aaec720da0bfaceeb1f26b0b348
EstefaniaG19 <101020959+EstefaniaG19@users.noreply.github.com>
2022-03-06 10:47:56 -0500

    first commit

---
+++ Cuenta_bancaria/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Cuenta_bancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("   | Cuenta Bancaria |  ");
+            Console.WriteLine("-------------------------");
+
+            cuenta_bancaria Banco = new cuenta_bancaria ();
+            Console.WriteLine("  N° Cuenta : ");
+            Banco.Numero_cuenta = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(" Nombre : ");
+            Banco.Nombre_cliente = Console.ReadLine();
+            Console.WriteLine(" Saldo Actual : ");
+            Banco.Saldo_actual = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Interes Anual");
+            Banco.Interes = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("********** Los Datos Registrados son*************");
+            Console.WriteLine(" Numero de cuenta : "+Banco.Numero_cuenta);
+            Console.WriteLine(" Nombre del propietario de la cuenta : "+Banco.Nombre_cliente);
+            Console.WriteLine(" Saldo actual de la cuenta : "+Banco.Saldo_actual);
+            Console.WriteLine(" Interes Anual : "+Banco.Interes + "%\n");
+            Banco.Cliente("Lizbeth");
+            Console.WriteLine("-----------------------------------------------------");
+            // constructor con parametros 
+
+            cuenta_bancaria banca1 = new cuenta_bancaria(2205045196 , " Estefania ", 630, 4.2);
+            Console.WriteLine("Numero de cuenta: "+banca1.Numero_cuenta);
+            Console.WriteLine("Nombre del propietario: " + banca1.Nombre_cliente);
+            Console.WriteLine(" Saldo actual: " + banca1.Saldo_actual);
+            Console.WriteLine(" Interes Anual : " + banca1.Interes + "%\n");
+          banca1.Cliente(" Brenda ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cuenta_bancaria/cuenta_bancaria.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cuenta_bancaria
+{
+    public class cuenta_bancaria
+    {
+        public long Numero_cuenta { set; get; }
+        public string Nombre_cliente { set; get; }
+        public long Saldo_actual { set; get; }
+        public double Interes { set; get; }
+
+        public cuenta_bancaria()
+        {
+        }
+
+        public cuenta_bancaria(long numero_cuenta, string nombre_cliente, long saldo_actual, double interes)
+        {
+            Numero_cuenta = numero_cuenta;
+            Nombre_cliente = nombre_cliente;
+            Saldo_actual = saldo_actual;
+            Interes = interes;
+        }
+
+        public void Cliente(string nombre_cliente)
+        {
+             if (Nombre_cliente.Equals(nombre_cliente))
+            {
+                Console.WriteLine("El nombre del cliente es igual al nombre registrado ");
+            }
+            else
+            {
+                Console.WriteLine("El nombre del cliente no es igual al nombre del cliente registrado ");
+            }
+        }
+
+       
+    }
+
+       
+
+
+
+
+}
